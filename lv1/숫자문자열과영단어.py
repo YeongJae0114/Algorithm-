376666bc984b27291c210394abb6b3ff4c36d633
YeongJae0114 <yjl8628@naver.com>
2023-07-16 13:45:15 +0900

    dic

---
+++ lv1/숫자문자열과영단어.py
@@ -0,0 +1,21 @@
+def solution(s):
+    s_dic = {
+        'zero' : '0',
+        'one' : '1',
+        'two' : '2',
+        'three' : '3',
+        'four' : '4',
+        'five' : '5',
+        'six' : '6',
+        'seven' : '7',
+        'eight' : '8',
+        'nine' : '9' 
+   }
+
+    for i in s_dic.keys():
+        if s.find(i) != -1:
+            s=(s.replace(i,s_dic[i]))
+
+    return int(s)
+
+print(solution("one4seveneight"))
