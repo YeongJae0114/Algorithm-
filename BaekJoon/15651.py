224599e5fe60594c04ffaa01afed0ddf8a1d6f40
YeongJae0114 <yjl8628@naver.com>
2023-09-12 09:54:03 +0900

    backtracking3

---
+++ BaekJoon/15651.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+a = 1 
+def back():
+    if len(check) == m:
+        #print(' '.join(map(str, check)))
+        for i in range(m):
+            print(check[i], end=" ")
+        print()
+        return
+    else:
+        for i in range(1,n+1):
+            check.append(i)
+            back()
+            check.pop()
+
+n, m = map(int, input().split())
+check = []
+back()
