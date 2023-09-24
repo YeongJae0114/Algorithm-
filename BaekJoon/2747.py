4cf795cbd1a7f56f267069e30a5ed84e7c3cbb58
YeongJae0114 <yjl8628@naver.com>
2023-09-24 13:11:02 +0900

    피보나치

--- BaekJoon/2747.py
+++ BaekJoon/2747.py
@@ -8,8 +8,9 @@ def fib1(n): # 재귀
         return 0
     elif n == 1 or n == 2:
         return 1
-    else:
+    else: 
         return fib1(n - 1) + fib1(n - 2)
+
 
 def fib2(n): # 단순 반복
     a, b = 0, 1
