5372c63ed763ba92a6c3cbffa1b61a65dab12816
YeongJae0114 <yjl8628@naver.com>
2023-10-11 11:58:30 +0900

    test 파일

--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,10 +1,2 @@
-from collections import deque
-que = deque()
+graph = []
 
-x1,y1,x2,y2 = map(int,input().split())
-graph=[]
-
-que.append((x1,y1))
-que.append((x2,y2))
-x,y=que.popleft()
-print(x,y)
