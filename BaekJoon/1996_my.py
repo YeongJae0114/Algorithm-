943a57f9ee1648468ccd4657a7391cd82f9ff1a0
YeongJae0114 <yjl8628@naver.com>
2023-10-12 22:23:54 +0900

    오류 수정

--- BaekJoon/1996_my.py
+++ BaekJoon/1996_my.py
@@ -4,39 +4,31 @@ from collections import deque
 n = int(input())
 
 graph = []
+new_graph = [[0] * n for _ in range(n)]
+
 que = deque()
 
 for _ in range(n):
-    a=list(input())
-    for i in range(len(a)):
-        if a[i]=='.':
-            a[i]=0
-        else:
-            a[i]=int(a[i])
-    graph.append(a)
-
-
+    graph.append(list(input()))
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] != 0:
-            que.append((i,j,graph[i][j]))
+        if graph[i][j] != ".":
+            que.append((i, j, graph[i][j]))
+            new_graph[i][j] = "*"
+
 while que:
-    y,x,k= que.popleft()
+    y, x, k = que.popleft()
 
-    for i in range(y-1,y+2):
-        for j in range(x-1,x+2):
+    for i in range(y - 1, y + 2):
+        for j in range(x - 1, x + 2):
             if -1 < i < n and -1 < j < n:
-                if i==y and j==x:
-                    graph[i][j]='*'
-                elif graph[i][j]=='*':
-                    continue
-                else:
-                    graph[i][j]=graph[i][j]+k
-                    if graph[i][j]>9:
-                        graph[i][j]='M'
+                if i == y and j == x:
+                    new_graph[i][j] = "*"
+                elif new_graph[i][j] != "M" and new_graph[i][j] != "*":
+                    new_graph[i][j] = new_graph[i][j] + int(k)
+                    if new_graph[i][j] > 9:
+                        new_graph[i][j] = "M"
 
-for i in (graph):
-    print()
-    for j in i:
-        print(j, end='')
+for i in new_graph:
+    print("".join(map(str, i)))
