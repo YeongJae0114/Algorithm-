35a1db4bb8a9ecfe3c1c29649e73ff842f45bdbf
YeongJae0114 <112526208+YeongJae0114@users.noreply.github.com>
2023-12-12 20:50:21 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,10 +1,8 @@
 def solution(my_string, target):
-    if 100 > len(my_string) >= 0 & 100 > len(target) >= 0: 
-        return
-    # target 문자열이 my_strung에 포함되지 않는다면 다음 문자 실행
+    # target 문자열이 my_strung에 포함되지 않는다면 다음 문장 실행
     if target not in my_string:
         answer = 0
-    else:
+    else:  # 반대의 경우 다음 문장 실행
         answer = 1
 
     return answer
