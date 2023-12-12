fcc6f094c6fd1312ac109d1ad02c93ea25558d7d
YeongJae0114 <112526208+YeongJae0114@users.noreply.github.com>
2023-12-12 20:51:04 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,10 @@
 def solution(age):
+    # 자릿수 별로 나눠 list 형식으로 저장  ex)[1,2,3]
     age = list(map(int, str(age)))
-    # 97 ~ 106
-    answer = ""
+    # 아스키코드 10진수 : a ~ j -> 97 ~ 106
+    
+    answer = "" # 변환한 행성의 나이를 저장할 변수
     for i in age:
+        # 변환할 숫자에 아스키 코드 값을 더해서 다시 문자로 변환
         answer += chr(i + 97)
     return answer
