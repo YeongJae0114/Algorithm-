86540d6d78240099e87cc2238269ec6a801f9b73
YeongJae0114 <112526208+YeongJae0114@users.noreply.github.com>
2023-12-12 20:51:28 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 876.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,4 +1,3 @@
-
 import math
 
 def solution(r1, r2):
@@ -6,12 +5,13 @@ def solution(r1, r2):
     # 바깥쪽 원의 x좌표를 나타내는 i
     for i in range(1, r2 + 1):
         # 원의 방정식을 이용해 y값을 찾음
-        y_r2 = math.sqrt(r2**2 - i**2) # y_r1 = (r2**2 - i**2)**(0.5)
+        y_r2 = math.sqrt(r2**2 - i**2) # y_r2 = (r2**2 - i**2)**(0.5)
+
         if i <= r1: # r1보다 큰 범위만 계산
              # 원의 방정식을 이용해 y값을 찾음
-            y_r1 = math.sqrt(r1**2 - i**2) # x_r2 = (r1**2 - i**2)**(0.5)
+            y_r1 = math.sqrt(r1**2 - i**2) # y_r1 = (r1**2 - i**2)**(0.5)
         else:
             y_r1 = 0
-        # floor : 내림, ceil : 올림
-        answer += math.floor(y_r2) - math.ceil(y_r1) + 1
+        
+        answer += math.floor(y_r2) - math.ceil(y_r1) + 1 # floor : 내림, ceil : 올림
     return answer * 4
