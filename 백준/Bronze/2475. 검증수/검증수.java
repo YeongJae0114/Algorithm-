6580a3c98caa99ac1fcccd135240e7650808e27e
YeongJae0114 <112526208+YeongJae0114@users.noreply.github.com>
2024-05-18 17:40:20 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f;
+		
+		f = (a * a + b * b + c * c + d * d + e * e) % 10;
+		System.out.println(f);
+		sc.close();
+	}
+}
