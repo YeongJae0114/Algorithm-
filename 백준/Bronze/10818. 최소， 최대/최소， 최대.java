be1349ac79211f7d69cb9689abe09ed96b53f4dc
yeongJae Lee <112526208+YeongJae0114@users.noreply.github.com>
2024-06-10 15:01:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 492 ms, Memory: 87688 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        
+        int max = -1000000;
+        int min = 1000000;
+        int num;
+        for (int i = 0; i < N; i++) {
+            num = Integer.parseInt(st.nextToken());
+            if (max < num)
+                max = num;
+            if (min > num)
+                min = num;
+        }
+        sb.append(min).append(" ").append(max);
+        System.out.println(sb);
+    }
+}
