4c9e021934b4ba50c8185f05c494c3113bfff4f8
yeongJae Lee <112526208+YeongJae0114@users.noreply.github.com>
2024-08-24 15:18:14 +0900

    [goormlevel] Title: 대소문자 바꾸기, Time: 0.16 ms, Memory: 33.25 MB, Difficulty: 0 -BaekjoonHub

---
+++ goormlevel/174732/1. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        char[] ch = input.toCharArray();
+        for (char c: ch) {
+            if (Character.isUpperCase(c)) {
+                c = Character.toLowerCase(c);
+            } else {
+                c = Character.toUpperCase(c);
+            }
+            System.out.print(c);
+        }
+	}
+}
