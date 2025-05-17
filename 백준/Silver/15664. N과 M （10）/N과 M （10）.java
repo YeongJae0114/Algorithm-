97e92b73a6477a49223b338b6a1f906c0bb6df72
yeongJae Lee <112526208+YeongJae0114@users.noreply.github.com>
2025-05-17 16:42:48 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+class Main{
+    static int N, M;
+    static int[] arr, map;
+    static StringBuffer sb = new StringBuffer();
+    static boolean visited[];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N];
+        arr = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            map[i] =  Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(map);
+
+        bt(0,0);
+        System.out.println(sb);
+        br.close();
+    }
+
+    static void bt(int depth, int start){
+        if(depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+
+        }
+        int prev = -1;
+        for(int i=start; i<map.length; i++){
+            if(map[i] == prev) continue;
+            arr[depth] = map[i];
+            prev = map[i];
+            bt(depth+1, i+1);
+        }
+    }
+}
