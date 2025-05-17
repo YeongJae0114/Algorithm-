4a29770d93308be6dedb4c923e2cdb1ca5a4fa8e
yeongJae Lee <112526208+YeongJae0114@users.noreply.github.com>
2025-05-17 13:42:43 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17016 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,36 +1,37 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import java.util.StringTokenizer;
 
 class Main{
-    static int N, M;
+    static int N,M;
     static int[] arr;
     static StringBuffer sb = new StringBuffer();
-
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        
         arr = new int[M];
         
         bt(0,0);
         System.out.println(sb);
+        br.close();
     }
     
-    static void bt(int depth, int at){
-        if(depth==M){
+    static void bt(int depth, int start){
+        if(depth == M){
             for(int val : arr){
                 sb.append(val).append(' ');
             }
             sb.append('\n');
             return;
         }
-        
-        for(int i=at; i<N; i++){
+        for(int i=start; i<N; i++){
             arr[depth] = i+1;
             bt(depth+1, i);
         }
