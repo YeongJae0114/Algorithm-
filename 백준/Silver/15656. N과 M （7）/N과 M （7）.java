e72a2bb0c7aa824426633dbdbd0e3936b633b135
yeongJae Lee <112526208+YeongJae0114@users.noreply.github.com>
2025-05-17 14:52:08 +0900

    [Silver III] Title: N과 M (7), Time: 656 ms, Memory: 124044 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main{
+    static int N, M;
+    static int[] arr, map;
+    static StringBuffer sb = new  StringBuffer();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[M];
+        map = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(map);
+        
+        bt(0,0);
+        System.out.println(sb);
+        br.close();
+    }
+    
+    static void bt(int depth, int start){
+        if(depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i=0; i<N; i++){
+            arr[depth] = map[i];
+            bt(depth+1, i);
+        }
+    }
+}
